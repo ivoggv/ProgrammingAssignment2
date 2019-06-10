7793272d11d9765f1d96203544858ae7fcd0d1d7
Ivo Giulietti <ivog@asafondos.org.sv>
2019-06-10 17:27:50 -0600

    Ivo Giuliano Giulietti Vega Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is my first attempt doing this assignment. The first function will
+## create a cache matrix based on the info I set.
+## 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() {
+                x
+        }
+        setinv <- function(inv) m<<-inv
+        getinv <- function()m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Given the last function then this one gets the information from the 
+## previous chunk of code. IF there is no info then it proceeds to calculate 
+## its own. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getinv()
+        if (!is.null(m)) {
+                message("Getting chached matrix")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setinv(m)
+        m
 }
